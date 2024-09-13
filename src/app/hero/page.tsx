a757d985d828308e536e570f116ca26130aeaa9c
inehadev <156919946+inehadev@users.noreply.github.com>
2024-09-13 21:45:44 +0530

    update with ui

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
+import { House } from 'lucide-react';
 const hero = () => {
   return (
-    <div className='flex min-h-screen justify-center  ' >
+    <div className='flex min-h-screen justify-center ' >
 
         <div className='bg-blue-600 w-[300px]'>
             {/*   sidebar   */}
             <h2 className='text-2xl  text-white font-bold mt-5 ml-5'>IndiaRoot </h2>
              
-            <div className='flex mt-3 mx-4' > <Input placeholder='Search ' className='bg-none'>
-            </Input><Search className='text-white' size={35}/>
+            <div className='flex mt-3 mx-4  text-white rounded-full border ' > <Search className='text-white ml-2 mt-1' size={30}/><Input placeholder='Search ' className='bg-transparent border-none placeholder:text-white'>
+            </Input>
+            </div>
+
+            <div>
+            <House /> <span className='flex'>Feed</span>
+              
             </div>
             
            
