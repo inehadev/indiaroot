7032deffa55886bb14f40f2eb52c88614c243571
inehadev <156919946+inehadev@users.noreply.github.com>
2024-09-22 13:48:00 +0530

    postmodel

---
+++ src/app/models/postmodel.ts
@@ -0,0 +1,56 @@
+
+import mongoose, { Document, Schema, models } from "mongoose";
+
+interface postDocument extends Document {
+    postedBy: mongoose.Schema.Types.ObjectId,
+    likes: mongoose.Schema.Types.ObjectId
+    text: string,
+    replies: string
+    img:string
+}
+
+const postSchema = new Schema<postDocument>({
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {
+        type: String,
+        maxlength:500
+    },
+    img:{
+        type:String
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    ],
+    replies: [
+        {
+            userID: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+
+            },
+            text:{
+                type:String,
+                required:true
+            },
+            userProfiePic:{
+                type:String
+            },
+            username:{
+                type:String
+            }
+
+
+        }
+    ]
+})
+
+export const Post = models.Post || mongoose.model<postDocument>("Post" , postSchema)
