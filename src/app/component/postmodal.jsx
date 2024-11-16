18bb65514429157c81c365fb787908b5b769a238
nehha0 <neehhaa0@gmail.com>
2024-11-16 22:27:33 +0530

    update with postmodel

--- src/app/component/postmodal.jsx
+++ src/app/component/postmodal.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React , {useState} from 'react'
+import React , {useRef, useState} from 'react'
 import { Input } from '@/components/ui/input'
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from '@/components/ui/button'
@@ -7,6 +7,7 @@ import { X } from 'lucide-react';
 
 
 const Postmodal = ({onclose}) => {
+  const postref = useRef();
     
 
 
